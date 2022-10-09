11df7b8ea794dfcc4e3bc3bcb0dfe582843d80e0
rajjirajneesh <115339428+rajjirajneesh@users.noreply.github.com>
2022-10-09 17:39:19 +0530

    Add files via upload

---
+++ lnbcal.py
@@ -0,0 +1,21 @@
+while(1):
+    a=int(input('enter 1st number'))
+    b=int(input('enter 2nd number'))
+    product=a*b
+    print('product of 1st and 2nd number=',product)
+    if(product>500):
+        print('sum of 1st and 2nd number=',a+b)
+    else:
+        print('hello lnb code is running fine')
+
+        
+    
+
+    
+    
+
+
+
+
+
+
